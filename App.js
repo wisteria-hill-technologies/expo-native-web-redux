7fe58f29697bae5ab8ae21c581a0e2575ffab031
Noby Fujioka <n.fujioka@abacusmaths.info>
2020-05-06 01:00:53 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [ outputText, setOutputText ] = useState('Change text');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, world!</Text>
+      <Button
+        title={outputText}
+        onPress={() => setOutputText('You pressed!')}
+      />
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+# Expo Native Web WITH redux
+
+I am going to build a sample expo app who can run on ios and android devices as well as web.
+
+Todos:
+1. Refresh react native basics (It was 1-2 years ago since I used react native last time).
+2. Use react router for web navigation
+3. Use redux (also refresher. After this, I am going to use apollo client instead).
+4. Connect to an api
+5. Add authentication
